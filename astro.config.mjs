b1cb55dabe20a1cedf86004f7fc9bb51f77af15f
Renato Mercado <renatomercadoluna@hotmail.com>
2026-04-22 23:15:41 -0500

    update in UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/anything': '/'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
